db88ad5c016d854b253097e96472f8fe59cfbfe5
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-01-21 20:48:18 -0500

    Deployment to Heroku

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially set up to equal to a "dummy" customer.
+// But it could have been an empty array as well.
+// ===============================================================================
+var friendsArray = [
+    {
+      name: "MikeOwen",
+      photo: "https://media.giphy.com/media/1skTYzBWynK1y/giphy.gif",
+      scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
+    },
+    {
+      name: "Pavan",
+      photo: "https://media.giphy.com/media/7q9LxggEzRiOk/giphy.gif",
+      scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"]
+    },
+    {
+      name: "Angel",
+      photo: "https://media.giphy.com/media/AfpEltQudBg5y/giphy.gif",
+      scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"]
+    },
+    {
+      name: "Karina",
+      photo: "https://media.giphy.com/media/8SCntm1JiCS2s/giphy.gif",
+      scores: ["5", "1", "2", "1", "3", "4", "1", "3", "3", "4"]
+    },
+    {
+      name: "Tommy",
+      photo: "https://media.giphy.com/media/xOBhCxh7Dk6SA/giphy.gif",
+      scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"]
+    },
+    {
+      name: "Lisa",
+      photo: "https://media.giphy.com/media/2ca9aemQxIQXS/giphy.gif",
+      scores: ["1", "1", "5", "1", "3", "2", "5", "1", "1", "5"]
+    },
+    {
+      name: "Nick",
+      photo: "https://media.giphy.com/media/9OnUuWyjZ4VSU/giphy.gif",
+      scores: ["5", "4", "4", "2", "4", "4", "3", "2", "3", "3"]
+    },
+    {
+      name: "Jason",
+      photo: "https://media.giphy.com/media/9M36yTC8BlRPG/giphy.gif",
+      scores: ["5", "2", "3", "1", "5", "1", "3", "1", "1", "5"]
+    },
+    {
+      name: "Nate",
+      photo: "https://media.giphy.com/media/6mwSgAGomAo7K/giphy.gif",
+      scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+    {
+      name: "Ken",
+      photo: "https://media.giphy.com/media/tTLvTaZruCBoc/giphy.gif",
+      scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    }
+  ];
+  
+  //Below to export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
